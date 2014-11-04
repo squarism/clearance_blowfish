1b6a189f6dd5267526e883af32c5517a0bd22179
Chris Dillon <squarism@gmail.com>
2014-11-03 16:59:09 -0800

    fix encoding issue on database save

--- lib/clearance_blowfish.rb
+++ lib/clearance_blowfish.rb
@@ -28,6 +28,7 @@ module Clearance
         cipher = OpenSSL::Cipher::Cipher.new('bf-cbc').encrypt
         cipher.key = Digest::SHA256.digest(salt)
         cipher.update(string) << cipher.final
+        Base64.encode64(string).encode('utf-8')
       end
 
       def initialize_salt_if_necessary
@@ -37,7 +38,7 @@ module Clearance
       end
 
       def generate_salt
-        SecureRandom.hex(20).encode('UTF-8')
+        Base64.encode64(SecureRandom.hex(20)).encode('utf-8')
       end
     end
   end
